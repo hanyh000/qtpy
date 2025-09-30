1205e408977543bc0dc5143fa5bdb2da639f1a7e
hanyh <hanyh000@naver.com>
2025-09-30 17:48:52 +0900

    app수정과 pd_output작성

--- fruit_app.py
+++ fruit_app.py
@@ -6,10 +6,6 @@ from fruit_pd_output import MainWindow
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    login = inputinfo()
-    if login.exec_() == inputinfo.Accepted:
-        w = MainWindow()
-        w.show()
-        sys.exit(app.exec_())
-    else:
-        sys.exit(0)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec_())
